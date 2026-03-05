17c8bf017a232a98e8d1fb5c078f02958e753346
reza luna 2501091005 <rezaluna2313@gmail.com>
2026-03-05 13:24:39 +0700

    Update RelasiDemo.java

--- RelasiDemo.java
+++ RelasiDemo.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Tazkiya_050626;
+package Reza Luna;
 
 /**
  *
@@ -48,3 +48,4 @@ System.out.println(" i != j = " + (i != j)); //true
 System.out.println(" k != j = " + (k != j)); //false
    }
 }
+
