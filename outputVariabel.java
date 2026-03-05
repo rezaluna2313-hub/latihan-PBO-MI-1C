0d38d40b3cffa4361c3d3e6572923338b5656b5c
reza luna 2501091005 <rezaluna2313@gmail.com>
2026-03-05 13:25:26 +0700

    Update outputVariabel.java

--- outputVariabel.java
+++ outputVariabel.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Tazkiya_050626;
+package Reza Luna;
 
 /**
  *
@@ -17,3 +17,4 @@ public class outputVariabel {
         System.out.println("The Value of x = " +x );
   }  
 }
+
