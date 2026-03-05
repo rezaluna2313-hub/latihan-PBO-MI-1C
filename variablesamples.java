1a77157d1a00e42586632683caeb295671250d67
reza luna 2501091005 <rezaluna2313@gmail.com>
2026-03-05 13:25:43 +0700

    Update variablesamples.java

--- variablesamples.java
+++ variablesamples.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Tazkiya_050626;
+package Reza Luna;
 
 /**
  *
@@ -27,3 +27,4 @@ public class Variablesamples {
         System.out.println(grade);
     }
 }
+
